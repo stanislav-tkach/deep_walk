8de451a9b454e56997230ccb6e0d1ea56348d87b
DarkEld3r <stanislav.tkach@gmail.com>
2016-11-13 14:56:14 +0200

    Work in progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,43 @@
-struct DeepWalk {
+use std::path::{Path, PathBuf};
+
+pub struct DeepWalk {
+    root: PathBuf,
+}
+
+impl DeepWalk {
+    pub fn new<P: AsRef<Path>>(root: P) -> Self {
+        DeepWalk { root: root.as_ref().to_path_buf() }
+    }
+}
+
+impl IntoIterator for DeepWalk {
+    type Item = Result<Entry, Error>;
+    type IntoIter = Iter;
+
+    fn into_iter(self) -> Iter {
+        Iter {
+        }
+    }
+}
+
+pub struct Iter {
+    //
+}
+
+impl Iterator for Iter {
+    type Item = Result<Entry, Error>;
+
+    fn next(&mut self) -> Option<Result<Entry, Error>> {
+        None
+    }
+}
+
+pub struct Entry {
+    //
+}
+
+pub struct Error {
+    //
 }
 
 #[cfg(test)]
